2225b58af8b3cb8d2714705757d0e5ef93fd1cb3
Linus Widing <l.widing@web.de>
2024-09-04 11:35:44 +0200

    Include todo router

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ load_dotenv()
 
 from fastapi import FastAPI
 from app import models
-from app.routes import auth, user
+from app.routes import auth, user, todo
 from app.db import engine
 
 
@@ -13,3 +13,4 @@ models.Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
 app.include_router(user.router)
+app.include_router(todo.router)
